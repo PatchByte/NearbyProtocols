93fc9855718d35cc202c1c5c5a55b12e783dbd60
Hendrik A. <patchbyte@ash.local>
2024-07-03 01:08:56 +0200

    protocols: format

--- NearbyProtocols/include/NearbyProtocols/ShareAdvertisement.h
+++ NearbyProtocols/include/NearbyProtocols/ShareAdvertisement.h
@@ -16,19 +16,21 @@ enum nearby_share_advertisement_version
 struct nearby_share_advertisement
 {
     enum nearby_share_advertisement_version version;
-    union {
+
+    union
+    {
         bool has_name;
         bool is_visible;
     };
     enum nearby_share_target_type device_type;
-    unsigned char salt[NEARBY_SHARE_ADVERTISEMENT_SALT_SIZE];
-    unsigned char metadata_encryption_key_hash_byte[NEARBY_SHARE_ADVERTISEMENT_METADATA_ENCRYTION_KEY_HASH_BYTE_SIZE];
-    unsigned char name_length;
-    char* name_data;
+    unsigned char                 salt[NEARBY_SHARE_ADVERTISEMENT_SALT_SIZE];
+    unsigned char                 metadata_encryption_key_hash_byte[NEARBY_SHARE_ADVERTISEMENT_METADATA_ENCRYTION_KEY_HASH_BYTE_SIZE];
+    unsigned char                 name_length;
+    char*                         name_data;
 };
 
 const char* nearby_share_advertisement_version_to_string(enum nearby_share_advertisement_version version);
-void nearby_share_advertisement_from_endpoint_info(struct nearby_share_advertisement* advertisement, struct nearby_utils_buffer* buffer);
-void nearby_share_advertisement_from_endpoint_info_cleanup(struct nearby_share_advertisement* advertisement);
+void        nearby_share_advertisement_from_endpoint_info(struct nearby_share_advertisement* advertisement, struct nearby_utils_buffer* buffer);
+void        nearby_share_advertisement_from_endpoint_info_cleanup(struct nearby_share_advertisement* advertisement);
 
 #endif // !NEARBYPROTOCOLS_SHAREADVERTISEMENT_H
